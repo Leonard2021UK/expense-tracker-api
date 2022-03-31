8d19bf410efe232874d58111f3125bc9e576585c
leo <nema.otto@gmal.com>
2022-03-31 21:02:49 +0100

    add CORS mapping to MvcConfiguration.java

--- src/main/java/com/codercampus/api/security/MvcConfiguration.java
+++ src/main/java/com/codercampus/api/security/MvcConfiguration.java
@@ -5,7 +5,11 @@ import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 // sets SameSite cookie attribute to NONE
 @Configuration
@@ -17,5 +21,15 @@ public class MvcConfiguration implements WebMvcConfigurer {
             cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
             context.setCookieProcessor(cookieProcessor);
         };
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://127.0.0.1:3000","https://https://expense-tracker-client-2022.herokuapp.com/","https://localhost:3000")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("Set-Cookie","Authorization", "Content-Type","Accept")
+                .maxAge(3600);
     }
 }
