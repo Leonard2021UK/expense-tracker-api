3716ee24650befe21544cd00ebad3e84b21df474
leo <nema.otto@gmal.com>
2022-04-01 09:01:09 +0100

    add CORS mapping to MvcConfiguration.java

--- src/main/java/com/codercampus/api/security/WebSecurityConfig.java
+++ src/main/java/com/codercampus/api/security/WebSecurityConfig.java
@@ -102,7 +102,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("Set-Cookie","Authorization", "Content-Type","Accept"));
 //                configuration.setExposedHeaders(List.of("Set-Cookie"));
-                System.out.println(configuration);
+                System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDdddd");
                 return configuration;
 
             };
