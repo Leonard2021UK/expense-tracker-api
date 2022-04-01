1bda412555cf0ed546bbedcf098fa11864c52814
leo <nema.otto@gmal.com>
2022-04-01 09:06:23 +0100

    add CORS mapping to MvcConfiguration.java

--- src/main/java/com/codercampus/api/security/WebSecurityConfig.java
+++ src/main/java/com/codercampus/api/security/WebSecurityConfig.java
@@ -111,8 +111,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         });
 
 
-        http.cors();
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 
